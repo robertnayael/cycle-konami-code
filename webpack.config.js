ea74f7e391a4fd737fe29853cbe12965f4f371ed
Robert Gulewicz <nayael@wp.pl>
2019-03-27 00:40:19 +0100

    Create initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
+}
