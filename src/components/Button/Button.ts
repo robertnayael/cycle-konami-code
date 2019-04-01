81dcb927f5da48731e9206ecc65df7d36e879585
Robert Gulewicz <nayael@wp.pl>
2019-04-01 23:27:43 +0200

    Add button component

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,44 @@
+import { button, DOMSource, VNode } from '@cycle/dom'
+import { combineLatest, of, Observable } from 'rxjs'
+import { map, scan, share, shareReplay, startWith } from 'rxjs/operators'
+
+namespace Button {
+
+    export interface Props {
+        activeLabel: string
+        inactiveLabel: string
+    }
+
+    export interface Sources {
+        DOM: DOMSource,
+        keyup: Observable<string>,
+        props: Observable<Props>
+    }
+
+    export interface Sinks {
+        DOM: Observable<VNode>
+        isActive: Observable<boolean>
+    }
+}
+
+export function Button({ DOM, keyup: keyup$, props: props$ }: Button.Sources): Button.Sinks {
+
+    const click$ = (DOM.select('button').events('click') as unknown as Observable<Event>)
+    .pipe(share())
+
+    const isActive$ = click$.pipe(
+        startWith(false),
+        scan<any, boolean>((wasActive, _) => !wasActive),
+        shareReplay(1)
+    )
+
+    const DOM$ = combineLatest(isActive$, props$).pipe(
+        map(([ isActive, { activeLabel, inactiveLabel } ]) =>
+            button(isActive ? activeLabel : inactiveLabel))
+    )
+
+    return {
+        DOM: DOM$,
+        isActive: isActive$
+    }
+}
