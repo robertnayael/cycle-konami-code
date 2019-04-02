4695ce3d46e5cd62829e3b7083d4e811037a7925
Robert Gulewicz <nayael@wp.pl>
2019-04-02 23:36:29 +0200

    Describe tests

---
+++ src/components/Button/Button.test.ts
@@ -0,0 +1,21 @@
+import { Button } from './'
+
+describe('Button component', () => {
+
+    it('should create', () => {
+
+    })
+
+    it('should render a <button> element', () => {
+
+    })
+
+    it('should emit `true` / `false` values on toggle on and off', () => {
+
+    })
+
+    it('should switch labels on toggle on and off', () => {
+
+    })
+
+})
